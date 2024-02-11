9d9176ce589f5a4be61d9a81571d0ef8814e4b4c
Seandowling123 <dowling.sean103@gmail.com>
2024-02-11 18:41:32 +0000

    asf

--- Maze_generator.py
+++ Maze_generator.py
@@ -1,9 +1,16 @@
 from pyamaze import maze,COLOR,agent
 
 m=maze(5,5)
-m.CreateMaze()
-a=agent(m,footprints=True)
+m.CreateMaze(theme="light")
+a=agent(m,filled=True,footprints=True,color=COLOR.red)
+b=agent(m,filled=True,footprints=True,color=COLOR.cyan)
 
-path = 'EENWWSES'
+path = 'WWNWNNENW'
+path2 = 'WNWNWNNENW'
+m.tracePath({a:path}) 
 
+print(m.path)
 m.run()
+print(m.maze_map)
+
+# The plan: MAke 3 func BFS, DFS and A* return a string to solve the maze map
