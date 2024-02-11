784898c932edde46e2e8597d81e48e8fe1771791
Seandowling123 <dowling.sean103@gmail.com>
2024-02-11 17:49:50 +0000

    messing

---
+++ Maze_generator.py
@@ -0,0 +1,10 @@
+import pymaze
+
+# Create a maze generator object
+generator = mazegenerator.MazeGenerator()
+
+# Generate a maze with dimensions 10x10
+maze = generator.generate(10, 10)
+
+# Print the maze
+print(maze)
--- venv/Scripts/Maze_generator.py
+++
@@ -1,6 +0,0 @@
-from pymaze import maze
-
-m = maze.Maze(10, 10)  # Example: Creates a 10x10 maze
-m.generate()
-
-m.draw()
