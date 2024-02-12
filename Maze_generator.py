dbc4630e0cbc429fbe82da627d964354f82c1b97
Seandowling123 <dowling.sean103@gmail.com>
2024-02-12 11:11:52 +0000

    DFS

--- Maze_generator.py
+++ Maze_generator.py
@@ -1,20 +1,23 @@
 from pyamaze import maze, agent, COLOR
 
-m=maze(5,5)
+# Converts a path of cells to a string of directions
+def to_directions(path):
+    directions = ''
+    for i in range(1, len(path)):
+        current_y, current_x = path[i-1]
+        next_y, next_x = path[i]
 
-goal = (1,1)
-start = (5,5)
-
-m.CreateMaze(goal[0],goal[1],theme="light")
-a=agent(m,start[0],start[1],filled=True,footprints=True,color=COLOR.red)
-b=agent(m,filled=True,footprints=True,color=COLOR.cyan)
-
-path = 'WWNWNNENW'
-path2 = 'WNWNWNNENW'
-m.tracePath({a:path}) 
-
-#m.run()
-
+        if next_x > current_x:
+            directions += 'E'
+        elif next_x < current_x:
+            directions += 'W'
+        if next_y > current_y:
+            directions += 'S'
+        elif next_y < current_y:
+            directions += 'N'
+            
+    return directions
+    
 # Return a list of the available adjacent cells
 def get_available_cells(maze_map, current_pos, checked_cells):
     available_positions = []
@@ -36,37 +39,93 @@ def get_available_cells(maze_map, current_pos, checked_cells):
             available_positions.append(new_pos)
     return available_positions
 
+# Solve the maze with BFS
 def BFS(maze_map, start, goal):
     que = []
     checked_cells = []
     prev_pos = {}
     current_pos = start
-    que = que + get_available_cells(maze_map, current_pos, checked_cells)
-    print(que)
     
+    que = que + get_available_cells(maze_map, current_pos, checked_cells)
+    
+    # While the goal has not been reached & the que is not empty
     while((current_pos != goal) and (len(que) > 0)):
-        print(current_pos)
+        # Check neaby available cells
         checked_cells.append(current_pos)
         available_cells = get_available_cells(maze_map, current_pos, checked_cells)
         que = que + available_cells
         for cell in available_cells:
             prev_pos[cell] = current_pos
         
+        # Get next cell from the que
         current_pos = que[0]
-            
         del que[0]
-    
+        
+    # If the goal has been reached, get the path
     if current_pos == goal:    
         path = []
-        dummy = current_pos
-        path.append(dummy)
-        while dummy != start:
-            prev = prev_pos[dummy]
+        path.append(current_pos)
+        while current_pos != start:
+            prev = prev_pos[current_pos]
             path.append(prev)
-            dummy = prev
-        
-    return path[::-1]
+            current_pos = prev
+    
+    # Convert the path to a set of directions
+    path_reversed = path[::-1]
+    directions = to_directions(path_reversed)
+    return directions
 
+def DFS(maze_map, start, goal):
+    que = []
+    checked_cells = []
+    prev_pos = {}
+    current_pos = start
+    
+    que = que + get_available_cells(maze_map, current_pos, checked_cells)
+    
+    # While the goal has not been reached & the que is not empty
+    while((current_pos != goal) and (len(que) > 0)):
+        # Check neaby available cells
+        checked_cells.append(current_pos)
+        available_cells = get_available_cells(maze_map, current_pos, checked_cells)
+        que = que + available_cells
+        for cell in available_cells:
+            prev_pos[cell] = current_pos
+        
+        # Get next cell from the que
+        current_pos = que[len(que)]
+        del que[len(que)]
+        
+    # If the goal has been reached, get the path
+    if current_pos == goal:    
+        path = []
+        path.append(current_pos)
+        while current_pos != start:
+            prev = prev_pos[current_pos]
+            path.append(prev)
+            current_pos = prev
+    
+    # Convert the path to a set of directions
+    path_reversed = path[::-1]
+    directions = to_directions(path_reversed)
+    return directions
+
+
+# Set variables
+size = (8,8)
+goal = (1,1)
+start = (5,5)
+
+# Create maze & agents
+m=maze(size[0],size[1])
+m.CreateMaze(goal[0],goal[1],loopPercent=100,theme="light")
+BFS_agent=agent(m,start[0],start[1],filled=True,footprints=True,color=COLOR.cyan)
+DFS_agent=agent(m,start[0],start[1],filled=True,footprints=True,color=COLOR.red)
+
+path = BFS(m.maze_map, start, goal)
 print(BFS(m.maze_map, start, goal))
+m.tracePath({BFS_agent:path}) 
+
+m.run()
     
     
